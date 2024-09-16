2f956b767db2a8bdede26e63a5d17762f6a09700
Duncan Niemann <dniemann2020@gmail.com>
2024-09-16 16:33:38 +0800

    Add files via upload
    Adding a new worksheet foo school

---
+++ Assessment 1 worksheet 1.py
@@ -0,0 +1,53 @@
+print ("Hello Wolrd!")
+# Declare two variables, a and b
+a = 15
+b = 12
+
+# Print the types of a and b using the type() function
+print("Type of a:", type(a))
+print("type of b:", type(b))
+
+
+# create a code script to perform an addition (a + b) and print the result
+result = a + b
+print("the sum of a + b equals to", result)
+
+# Create a code script to perform a subtraction (a + b) and print the result
+result = a - b
+print("The difference of a - b equals to", result)
+
+# Create a code script to multiply (a * b) and print the results
+result = a * b
+print("The product of a * b equals to", result)
+
+# Create a code scrript to perform a division (a / b) and print the result
+result = a / b
+print("The quotient of a / b equals to", result)
+
+
+# Create a new variable c that stores the result of a divided by b. make sure c is of type integer
+c = int(a / b)
+print("Value of c:", a)
+print("Type of c:", type(c))
+
+# Convert c to a float and print its new value and type
+c = float(c)
+print("Value of c (float):", c)
+print("Type of c (float):, type(c)")
+
+
+# Declare a string variable message with the value "The result of divided by b is"
+message = "The result of a divided by b is"
+
+# Concatenate the message with the value of c (converted to a string) and print the result
+result_message = message + str(c)
+print(result_message)
+
+
+# Compare if a is greater than b and print the result (True/False)
+result = a > b
+print("Is a greater than b?", result)
+
+# Check if a is equal to b and print the result (True/Flase)
+result = a = b
+print("Is a equal to b?", result)
